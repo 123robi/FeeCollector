b8649c36735cd911b1cc438ee13e06150a99d2f8
Robert Kosir <rkosir@pc05.3sixty.local>
2018-09-05 11:32:18 +0200

    Navigation bar was added

--- app/src/main/java/com/feecollector/android/feecollector/Activity/DashboardActivity.java
+++ app/src/main/java/com/feecollector/android/feecollector/Activity/DashboardActivity.java
@@ -1,43 +1,70 @@
 package com.feecollector.android.feecollector.Activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
-import com.facebook.share.Share;
-import com.feecollector.android.feecollector.AppConfig;
 import com.feecollector.android.feecollector.Helper.TokenSaver;
 import com.feecollector.android.feecollector.R;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    private Button logout;
+	private Button logout;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+	private DrawerLayout drawerLayout;
+	private ActionBarDrawerToggle toggle;
+	private NavigationView navigationView;
 
-        logout = findViewById(R.id.logout);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dashboard);
+		/**
+		 * Navigation bar configuration
+		 */
+		drawerLayout = findViewById(R.id.activity_dashboard);
+		toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+		drawerLayout.addDrawerListener(toggle);
+		toggle.syncState();
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(AccessToken.getCurrentAccessToken() != null) {
-                    LoginManager.getInstance().logOut();
-                }
-                TokenSaver.setToken(DashboardActivity.this,false);
-                Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-    }
+		navigationView = findViewById(R.id.nav_view);
+		navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+			@Override
+			public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+				if(item.getItemId() == R.id.log_out) {
+					if(AccessToken.getCurrentAccessToken() != null) {
+						LoginManager.getInstance().logOut();
+					}
+					TokenSaver.setToken(DashboardActivity.this,false);
+					Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
+					startActivity(intent);
+					finish();
+				}
+				return true;
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(toggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
