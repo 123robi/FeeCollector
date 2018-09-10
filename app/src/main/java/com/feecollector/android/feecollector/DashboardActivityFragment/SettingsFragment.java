b459dcae2d9aeefc2357a221ce65c36089006743
Robert Kosir <rkosir@pc05.3sixty.local>
2018-09-10 14:12:22 +0200

    Merging

--- app/src/main/java/com/feecollector/android/feecollector/DashboardActivityFragment/SettingsFragment.java
+++ app/src/main/java/com/feecollector/android/feecollector/DashboardActivityFragment/SettingsFragment.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+<<<<<<< Updated upstream
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+=======
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
+>>>>>>> Stashed changes
 
 import com.feecollector.android.feecollector.R;
 
@@ -16,6 +21,7 @@ import com.feecollector.android.feecollector.R;
  */
 public class SettingsFragment extends Fragment {
 
+<<<<<<< Updated upstream
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -27,4 +33,16 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings,container,false);
     }
+=======
+	@Override
+	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+		Log.d("KEY", rootKey + "KEY");
+		setPreferencesFromResource(R.xml.pref_main, rootKey);
+	}
+
+	@Override
+	public Fragment getCallbackFragment() {
+		return this;
+	}
+>>>>>>> Stashed changes
 }
