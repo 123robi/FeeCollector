1f699af598e600c4f0dd10f899723cfd29905361
Robert Kosir <rkosir@pc05.3sixty.local>
2018-09-11 11:11:47 +0200

    Added Toolbar to SettingActivity and changed depriciated methods

--- app/src/main/java/com/feecollector/android/feecollector/Activity/SettingsActivity.java
+++ app/src/main/java/com/feecollector/android/feecollector/Activity/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.feecollector.android.feecollector.Activity;
 
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.v7.widget.Toolbar;
 
 import com.feecollector.android.feecollector.R;
@@ -12,10 +13,22 @@ public class SettingsActivity extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
-		addPreferencesFromResource(R.xml.pref_main);
 
 		toolbar = findViewById(R.id.back_action_bar);
+		toolbar.setTitle(R.string.settings);
 		toolbar.setNavigationOnClickListener(view -> onBackPressed());
+		getFragmentManager().beginTransaction().replace(R.id.content,new MyPreferenceFragment()).commit();
+
+	}
+
+	public static class MyPreferenceFragment extends PreferenceFragment
+	{
+		@Override
+		public void onCreate(final Bundle savedInstanceState)
+		{
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.pref_main);
+		}
 	}
 
 }
