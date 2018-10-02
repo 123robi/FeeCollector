d95cecb4e515887437a2af08f9b241bbc0f94750
Robert Kosir <rkosir@pc05.3sixty.local>
2018-10-02 10:24:48 +0200

    Change to CakePHP urls

--- app/src/main/java/eu/rkosir/feecollector/AppConfig.java
+++ app/src/main/java/eu/rkosir/feecollector/AppConfig.java
@@ -1,10 +1,10 @@
 package eu.rkosir.feecollector;
 
 public class AppConfig {
-	public static final String URL_LOGIN = "http://rkosir.eu/FeeCollector/login.php";
-	public static final String URL_REGISTER = "http://rkosir.eu/FeeCollector/register.php";
-	public static final String URL_CHANGE_PASSWORD = "http://rkosir.eu/FeeCollector/changePassword.php";
-	public static final String URL_CHANGE_PASSWORD_FACEBOOK = "http://rkosir.eu/FeeCollector/changePasswordFacebook.php";
+	public static final String URL_LOGIN = "http://rkosir.eu/Cake/usersApi/login";
+	public static final String URL_REGISTER = "http://rkosir.eu/Cake/usersApi/add";
+	public static final String URL_CHANGE_PASSWORD = "http://rkosir.eu/Cake/usersApi/changePassword";
+	public static final String URL_CHANGE_PASSWORD_FACEBOOK = "http://rkosir.eu/Cake/usersApi/changePasswordFacebook";
 
 	public static final String ALPHA_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	public static final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
