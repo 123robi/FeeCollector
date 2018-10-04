19b4328e21207158bfe2e9345f732e343340fcc5
Robert Košir <robikoser@gmail.com>
2018-10-04 23:01:27 +0200

    Changes in accessing php scripts

--- app/src/main/java/eu/rkosir/feecollector/AppConfig.java
+++ app/src/main/java/eu/rkosir/feecollector/AppConfig.java
@@ -1,10 +1,10 @@
 package eu.rkosir.feecollector;
 
 public class AppConfig {
-	public static final String URL_LOGIN = "http://rkosir.eu/Cake/usersApi/login";
-	public static final String URL_REGISTER = "http://rkosir.eu/Cake/usersApi/add";
-	public static final String URL_CHANGE_PASSWORD = "http://rkosir.eu/Cake/usersApi/changePassword";
-	public static final String URL_CHANGE_PASSWORD_FACEBOOK = "http://rkosir.eu/Cake/usersApi/changePasswordFacebook";
+	public static final String URL_LOGIN = "http://rkosir.eu/usersApi/login";
+	public static final String URL_REGISTER = "http://rkosir.eu/usersApi/add";
+	public static final String URL_CHANGE_PASSWORD = "http://rkosir.eu/usersApi/changePassword";
+	public static final String URL_CHANGE_PASSWORD_FACEBOOK = "http://rkosir.eu/usersApi/changePasswordFacebook";
 
 	public static final String ALPHA_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	public static final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
