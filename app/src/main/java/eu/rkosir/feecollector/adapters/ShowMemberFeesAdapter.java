ecd870e0e68038fcb624d680a66bc0aeaea80086
Robert Košir <robikoser@gmail.com>
2018-11-22 12:02:29 +0100

    Code Style Please enter the commit message for your changes. Lines starting

--- app/src/main/java/eu/rkosir/feecollector/adapters/ShowMemberFeesAdapter.java
+++ app/src/main/java/eu/rkosir/feecollector/adapters/ShowMemberFeesAdapter.java
@@ -23,76 +23,76 @@ import eu.rkosir.feecollector.entity.Place;
 import eu.rkosir.feecollector.helper.SharedPreferencesSaver;
 
 public class ShowMemberFeesAdapter extends RecyclerView.Adapter<ShowMemberFeesAdapter.ViewHolder> {
-    private List<MemberFee> membersFees;
-    private Context context;
-    private ShowMemberFeesAdapter.OnItemClickListener mListener;
-    private Place place;
+	private List<MemberFee> membersFees;
+	private Context context;
+	private ShowMemberFeesAdapter.OnItemClickListener mListener;
+	private Place place;
 
-    public interface OnItemClickListener {
-        void onItemClick(int position);
-    }
-    public void setOnItemClickListener(ShowMemberFeesAdapter.OnItemClickListener listener) {
-        mListener = listener;
-    }
+	public interface OnItemClickListener {
+		void onItemClick(int position);
+	}
+	public void setOnItemClickListener(ShowMemberFeesAdapter.OnItemClickListener listener) {
+		mListener = listener;
+	}
 
-    public ShowMemberFeesAdapter(List<MemberFee> membersFees, Context context) {
-        this.membersFees = membersFees;
-        this.context = context;
-    }
+	public ShowMemberFeesAdapter(List<MemberFee> membersFees, Context context) {
+		this.membersFees = membersFees;
+		this.context = context;
+	}
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView mFeeName;
-        public TextView mFeeAmount;
-        public TextView mFeeDate;
-        public TextView mFeeIsPaid;
+	public static class ViewHolder extends RecyclerView.ViewHolder {
+		public TextView mFeeName;
+		public TextView mFeeAmount;
+		public TextView mFeeDate;
+		public TextView mFeeIsPaid;
 
-        public ViewHolder(View itemView, ShowMemberFeesAdapter.OnItemClickListener listener) {
-            super(itemView);
-            mFeeName = itemView.findViewById(R.id.fee_name);
-            mFeeAmount = itemView.findViewById(R.id.fee_amount);
-            mFeeDate = itemView.findViewById(R.id.date);
-            mFeeIsPaid = itemView.findViewById(R.id.isPaid);
+		public ViewHolder(View itemView, ShowMemberFeesAdapter.OnItemClickListener listener) {
+			super(itemView);
+			mFeeName = itemView.findViewById(R.id.fee_name);
+			mFeeAmount = itemView.findViewById(R.id.fee_amount);
+			mFeeDate = itemView.findViewById(R.id.date);
+			mFeeIsPaid = itemView.findViewById(R.id.isPaid);
 
-            itemView.setOnClickListener(view -> {
-                if (listener != null) {
-                    int position = getAdapterPosition();
-                    if (position != RecyclerView.NO_POSITION) {
-                        listener.onItemClick(position);
-                    }
-                }
-            });
-        }
-    }
+			itemView.setOnClickListener(view -> {
+				if (listener != null) {
+					int position = getAdapterPosition();
+					if (position != RecyclerView.NO_POSITION) {
+						listener.onItemClick(position);
+					}
+				}
+			});
+		}
+	}
 
-    @NonNull
-    @Override
-    public ShowMemberFeesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_member_fee, parent, false);
-        ShowMemberFeesAdapter.ViewHolder viewHolder = new ShowMemberFeesAdapter.ViewHolder(v,mListener);
-        return viewHolder;
-    }
+	@NonNull
+	@Override
+	public ShowMemberFeesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_member_fee, parent, false);
+		ShowMemberFeesAdapter.ViewHolder viewHolder = new ShowMemberFeesAdapter.ViewHolder(v,mListener);
+		return viewHolder;
+	}
 
-    @Override
-    public void onBindViewHolder(@NonNull ShowMemberFeesAdapter.ViewHolder holder, int position) {
-        holder.mFeeName.setText(membersFees.get(position).getName());
-        holder.mFeeAmount.setText(membersFees.get(position).getAmount() + SharedPreferencesSaver.getCurrencySymbol(context));
-        if (membersFees.get(position).isPaid()) {
-            holder.mFeeIsPaid.setTextColor(context.getResources().getColor(R.color.green));
-            holder.mFeeIsPaid.setText(context.getResources().getString(R.string.user_detail_paid));
-        } else {
-            holder.mFeeIsPaid.setTextColor(context.getResources().getColor(R.color.red));
-            holder.mFeeIsPaid.setText(context.getResources().getString(R.string.user_detail_not_paid));
-        }
-        holder.mFeeDate.setText(getTimeFormat(membersFees.get(position).getDate()));
-    }
+	@Override
+	public void onBindViewHolder(@NonNull ShowMemberFeesAdapter.ViewHolder holder, int position) {
+		holder.mFeeName.setText(membersFees.get(position).getName());
+		holder.mFeeAmount.setText(membersFees.get(position).getAmount() + SharedPreferencesSaver.getCurrencySymbol(context));
+		if (membersFees.get(position).isPaid()) {
+			holder.mFeeIsPaid.setTextColor(context.getResources().getColor(R.color.green));
+			holder.mFeeIsPaid.setText(context.getResources().getString(R.string.user_detail_paid));
+		} else {
+			holder.mFeeIsPaid.setTextColor(context.getResources().getColor(R.color.red));
+			holder.mFeeIsPaid.setText(context.getResources().getString(R.string.user_detail_not_paid));
+		}
+		holder.mFeeDate.setText(getTimeFormat(membersFees.get(position).getDate()));
+	}
 
-    @Override
-    public int getItemCount() {
-        return membersFees.size();
-    }
+	@Override
+	public int getItemCount() {
+		return membersFees.size();
+	}
 
-    private String getTimeFormat(Calendar c) {
-        Date date = c.getTime();
-        return AppConfig.membersFeeFormat.format(date);
-    }
+	private String getTimeFormat(Calendar c) {
+		Date date = c.getTime();
+		return AppConfig.membersFeeFormat.format(date);
+	}
 }
