853b00f7bab94caa0f549e0f94c53b70f827a627
Robert Kosir <rkosir@pc05.3sixty.local>
2018-12-04 11:44:58 +0100

    Add Graph for 3 most fined users

--- app/src/main/java/eu/rkosir/feecollector/fragment/teamManagementFragment/Summary.java
+++ app/src/main/java/eu/rkosir/feecollector/fragment/teamManagementFragment/Summary.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,6 +24,18 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.applandeo.materialcalendarview.exceptions.OutOfDateRangeException;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -30,6 +43,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,13 +59,17 @@ import java.util.Locale;
 
 import eu.rkosir.feecollector.AppConfig;
 import eu.rkosir.feecollector.R;
+import eu.rkosir.feecollector.activity.teamManagement.UserDetail;
 import eu.rkosir.feecollector.adapters.ShowEventsAdapter;
+import eu.rkosir.feecollector.adapters.ShowMembersAdapter;
 import eu.rkosir.feecollector.entity.Event;
 import eu.rkosir.feecollector.entity.Place;
+import eu.rkosir.feecollector.entity.User;
 import eu.rkosir.feecollector.helper.SharedPreferencesSaver;
 import eu.rkosir.feecollector.helper.VolleySingleton;
 
 import static com.facebook.FacebookSdk.getApplicationContext;
+import static com.github.mikephil.charting.utils.ColorTemplate.VORDIPLOM_COLORS;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -65,6 +83,8 @@ public class Summary extends Fragment implements OnMapReadyCallback {
 	private String [] latlngArray;
 	private TextView mEventName, mEventDate, mEventTime, mEventDescription, mPlaceName;
 	private CardView mNextEvent;
+
+	private BarChart mChart;
 
 	public Summary() {
 		// Required empty public constructor
@@ -87,7 +107,8 @@ public class Summary extends Fragment implements OnMapReadyCallback {
 		mEventTime = view.findViewById(R.id.event_time);
 		mEventDescription = view.findViewById(R.id.event_description);
 		mPlaceName = view.findViewById(R.id.event_location);
-
+		mChart = view.findViewById(R.id.chart);
+		getMembers();
 		return view;
 	}
 
@@ -168,7 +189,64 @@ public class Summary extends Fragment implements OnMapReadyCallback {
 		RequestQueue requestQueue = VolleySingleton.getInstance(getApplicationContext()).getRequestQueue();
 		requestQueue.add(stringRequest);
 	}
+	private void getMembers() {
+		mProgressBar.setVisibility(View.VISIBLE);
+		String uri = String.format(AppConfig.URL_GET_TOP_3_FINED_USERS,
+				SharedPreferencesSaver.getLastTeamID(getApplicationContext()));
+		StringRequest stringRequest = new StringRequest(Request.Method.GET, uri, response -> {
+			JSONObject object = null;
+			try {
+				float startingPosition = 1f;
+				object = new JSONObject(response);
+					List<BarEntry> entries = new ArrayList<>();
+					ArrayList<String> names = new ArrayList<>();
+					names.add("blank");
+					int[]colors = {
+							ContextCompat.getColor(getApplicationContext(),R.color.first_graph),
+							ContextCompat.getColor(getApplicationContext(),R.color.second_graph),
+							ContextCompat.getColor(getApplicationContext(),R.color.third_graph)
+					};
+					BarDataSet[] sets = new BarDataSet[3];
+					JSONArray membersArray = object.getJSONArray("members");
+					for(int i = 0; i < membersArray.length(); i++) {
+						JSONObject user = membersArray.getJSONObject(i);
+						JSONObject matchingData = user.getJSONObject("_matchingData").getJSONObject("Users");
+						names.add(matchingData.getString("name"));
+						entries.add(new BarEntry(startingPosition,user.getInt("sum"),matchingData.getString("name")));
+						Log.d("NAME",matchingData.getString("name"));
+						startingPosition ++;
+					}
+				BarDataSet set = new BarDataSet(entries, "Stats");
+				set.setColors(VORDIPLOM_COLORS);
 
+				XAxis xAxis = mChart.getXAxis();
+				xAxis.setGranularity(1f);
+				xAxis.setGranularityEnabled(true);
+
+				BarData data = new BarData(set);
+				data.setBarWidth(0.9f);
+				data.setValueTextSize(12f);
+				mChart.setDescription(null);
+				mChart.setData(data);
+				mChart.setFitBars(true);
+				mChart.invalidate();
+				mChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(names));
+			} catch (JSONException e) {
+				Toast.makeText(getApplicationContext(),R.string.toast_unknown_error,Toast.LENGTH_LONG).show();
+				e.printStackTrace();
+			}
+		}, error -> {
+			Toast.makeText(getApplicationContext(),R.string.toast_unknown_error,Toast.LENGTH_LONG).show();
+		});
+
+		RequestQueue requestQueue = VolleySingleton.getInstance(getApplicationContext()).getRequestQueue();
+		requestQueue.add(stringRequest);
+		requestQueue.addRequestFinishedListener((RequestQueue.RequestFinishedListener<String>) request -> {
+			if (mProgressBar != null) {
+				mProgressBar.setVisibility(View.INVISIBLE);
+			}
+		});
+	}
 	private String getDateFormat(Calendar c) {
 		Date date = c.getTime();
 		return AppConfig.nextEventFormat.format(date) + " " + c.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
